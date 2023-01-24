f00ffef3b683b7ab2cff40347b5d3083560accf9
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-01-24 17:04:03 -0300

    Style

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-max-depth */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { useContext } from 'react';
 import FilterContext from '../context/FilterContext';
@@ -35,127 +36,135 @@ function Filter() {
   return (
     <div className="filter-container">
       <p>Filtros:</p>
-      <div className="filter-name-container">
-        <p>Nome:</p>
-        <input
-          className="filter-name"
-          data-testid="name-filter"
-          placeholder="Nome"
-          id="name-filter"
-          type="text"
-          value={ filterName.value }
-          onChange={ filterName.handleChange }
-        />
-      </div>
-      <div className="filter-column-sort-container">
-        <div className="filter-column-container">
-          <p>Coluna:</p>
-          <select
-            className="select-control"
-            data-testid="column-filter"
-            onChange={ columnFilter.handleChange }
-            disabled={ columnFilter.value === undefined }
-          >
-            {columnFilter.options
-              .map((item) => <option key={ item } value={ item }>{item}</option>)}
-          </select>
-
-          <select
-            className="select-control"
-            data-testid="comparison-filter"
-            onChange={ comparisonFilter.handleChange }
-          >
-            <option value="maior que">maior que</option>
-            <option value="menor que">menor que</option>
-            <option value="igual a">igual a</option>
-          </select>
-
+      <div className="filters">
+        <div className="filter-name-container">
+          <p>Nome:</p>
           <input
             className="filter-name"
-            data-testid="value-filter"
-            placeholder="Número"
-            type="number"
-            value={ valueFilter.value }
-            onChange={ valueFilter.handleChange }
+            data-testid="name-filter"
+            placeholder="Nome"
+            id="name-filter"
+            type="text"
+            value={ filterName.value }
+            onChange={ filterName.handleChange }
           />
-
-          <button
-            className="btn-add-remove"
-            data-testid="button-filter"
-            type="button"
-            onClick={ () => clickBtn() }
-            disabled={ columnFilter.value === undefined }
-          >
-            Add Filtro
-          </button>
-          <button
-            className="btn-add-remove"
-            data-testid="button-remove-filters"
-            type="button"
-            onClick={ clickBtnRemoveAll }
-          >
-            Remover todos
-          </button>
         </div>
-        <div className="filter-sort-container">
-          <p>Ordenar:</p>
-          <select
-            className="select-control"
-            data-testid="column-sort"
-            onChange={ sortFilter.handleChange }
-          >
-            {columnFilter.column
-              .map((item) => <option key={ item } value={ item }>{item}</option>)}
-          </select>
+        <div className="filter-column-sort-container">
+          <div className="filter-column-container">
+            <p>Coluna:</p>
+            <div className="select-container">
+              <select
+                className="select-control"
+                data-testid="column-filter"
+                onChange={ columnFilter.handleChange }
+                disabled={ columnFilter.value === undefined }
+              >
+                {columnFilter.options
+                  .map((item) => <option key={ item } value={ item }>{item}</option>)}
+              </select>
 
-          <div className="form-check radio-form">
+              <select
+                className="select-control"
+                data-testid="comparison-filter"
+                onChange={ comparisonFilter.handleChange }
+              >
+                <option value="maior que">maior que</option>
+                <option value="menor que">menor que</option>
+                <option value="igual a">igual a</option>
+              </select>
+            </div>
+
             <input
-              className="form-check-input"
-              data-testid="column-sort-input-asc"
-              name="sort"
-              id="column-sort-input-asc"
-              type="radio"
-              value="ASC"
-              onChange={ radioFilter.handleChange }
+              className="filter-name"
+              data-testid="value-filter"
+              placeholder="Número"
+              type="number"
+              value={ valueFilter.value }
+              onChange={ valueFilter.handleChange }
             />
-            <label className="form-check-label" htmlFor="column-sort-input-asc">
-              Ascendente
-            </label>
-          </div>
 
-          <div className="form-check radio-form">
-            <input
-              className="form-check-input"
-              data-testid="column-sort-input-desc"
-              name="sort"
-              id="column-sort-input-desc"
-              type="radio"
-              value="DESC"
-              onChange={ radioFilter.handleChange }
-            />
-            <label className="form-check-label" htmlFor="column-sort-input-desc">
-              Descendente
-            </label>
+            <button
+              className="btn-add-remove"
+              data-testid="button-filter"
+              type="button"
+              onClick={ () => clickBtn() }
+              disabled={ columnFilter.value === undefined }
+            >
+              Add Filtro
+            </button>
+            <button
+              className="btn-add-remove"
+              data-testid="button-remove-filters"
+              type="button"
+              onClick={ clickBtnRemoveAll }
+            >
+              Remover todos
+            </button>
           </div>
+          <div className="filter-sort-container">
+            <p>Ordenar:</p>
+            <div className="select-container">
+              <select
+                className="select-control"
+                data-testid="column-sort"
+                onChange={ sortFilter.handleChange }
+              >
+                {columnFilter.column
+                  .map((item) => <option key={ item } value={ item }>{item}</option>)}
+              </select>
 
-          <button
-            className="btn-add-remove"
-            data-testid="column-sort-button"
-            type="button"
-            onClick={ () => clickSort.handleClickSort({
-              column: sortFilter.value,
-              sort: radioFilter.value,
-            }) }
-          >
-            Ordenar
-          </button>
+            </div>
+
+            <div className="form-check radio-form">
+              <input
+                className="form-check-input"
+                data-testid="column-sort-input-asc"
+                name="sort"
+                id="column-sort-input-asc"
+                type="radio"
+                value="ASC"
+                onChange={ radioFilter.handleChange }
+              />
+              <label className="form-check-label" htmlFor="column-sort-input-asc">
+                Ascendente
+              </label>
+            </div>
+
+            <div className="form-check radio-form">
+              <input
+                className="form-check-input"
+                data-testid="column-sort-input-desc"
+                name="sort"
+                id="column-sort-input-desc"
+                type="radio"
+                value="DESC"
+                onChange={ radioFilter.handleChange }
+              />
+              <label className="form-check-label" htmlFor="column-sort-input-desc">
+                Descendente
+              </label>
+            </div>
+
+            <button
+              className="btn-add-remove"
+              data-testid="column-sort-button"
+              type="button"
+              onClick={ () => clickSort.handleClickSort({
+                column: sortFilter.value,
+                sort: radioFilter.value,
+              }) }
+            >
+              Ordenar
+            </button>
+          </div>
         </div>
       </div>
-      <div>
+      <div className="chosen-filters-container">
         {click.value.length > 0 && click.value.map((item, index) => (
-          <div key={ index } data-testid="filter">
+          <div className="chosen-filters" key={ index } data-testid="filter">
             <p>{ `${item[0]} ${item[1]} ${item[2]}` }</p>
             <button
+              className="button-remove"
               data-testid="button-remove"
               type="button"
               onClick={ () => clickBtnRemove(item) }
